f083c0195e44bfe7f10ca4cbc054755859e680bc
Spencer Killen <sjkillen@ualberta.ca>
2021-11-04 17:23:22 +0000

    Debug with assertions

--- 3.bash
+++ 3.bash
@@ -4,6 +4,6 @@
 mkdir /volume/llvm-project/build
 cd /volume/llvm-project/build
 
-CC=/usr/local/bin/clang CXX=/usr/local/bin/clang++ cmake ../llvm  -GNinja  -DLLVM_ENABLE_PROJECTS="clang;llvm;libcxx;libcxxabi;libclc;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX="/volume/llvm-install" -DLLVMPARALLEL_LINK_JOBS=12 -DLLVM_OPTIMIZED_TABLEGEN=ON -DBUILD_SHARED_LIBS=ON -DLLVM_USE_NEWPM=ON -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_INCLUDE_TESTS=OFF
+CC=/usr/local/bin/clang CXX=/usr/local/bin/clang++ cmake ../llvm  -GNinja  -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="clang;llvm;libcxx;libcxxabi;libclc;clang-tools-extra" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_INSTALL_PREFIX="/volume/llvm-install" -DLLVMPARALLEL_LINK_JOBS=12 -DLLVM_OPTIMIZED_TABLEGEN=ON -DBUILD_SHARED_LIBS=ON -DLLVM_USE_NEWPM=ON -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_INCLUDE_TESTS=OFF
 
 ninja -j12
